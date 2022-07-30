3da5df876e58da865f242713517cbe5a10993ec9
MarceloYudis <marceloyudisortiz@gmail.com>
2022-07-29 21:41:08 -0400

    prueba teorica de js

---
+++ probanding.js
@@ -0,0 +1,14 @@
+var nombresito = "Tadeo";
+var apeshido = "Juarez";
+var userPlatzi = "Tadeo";
+var ageUser = 22;
+var emailUser = "tadeoramirez@gmail.com"
+var overAge = true;
+var ahorros = 100000;
+var deudardas = -500;
+
+var completeNames = nombresito +" "+ apeshido;
+var totalMoney = ahorros - deudardas;
+
+completeNames;
+totalMoney;
