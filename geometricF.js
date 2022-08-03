523b3982208d21d60413a7eb558457347c00f93f
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-03 18:37:18 -0400

    area y perimetro del cuadrado y triangulo

--- geometricF.js
+++ geometricF.js
@@ -1,1 +1,33 @@
-console.log("HORA DEL CAFE")
+console.group()
+console.log("HORA DEL CAFE");
+
+let cuadradoLado = 9;
+console.log( "Cada lado del cuadrado mide: " + cuadradoLado + " mts")
+
+let perimetroCu = cuadradoLado * 4;
+
+console.log("El perimetro es de un total de: " + perimetroCu + " mts");
+
+let areaCu = cuadradoLado * cuadradoLado;
+
+console.log("Su area es de: "+ areaCu +" mts cuadrados")
+console.groupEnd();
+
+
+// Espacio del triangulo
+console.group()
+let trianguloLado = 6;
+let trianguloLado2 = 6;
+let trianguloBase = 4;
+let trianguloH = 7;
+
+console.log("Los lados del triangulo valen: " + trianguloLado + "cm, " + trianguloLado2 + "cm, " +
+trianguloBase + "cm. ")
+
+let perimetroTriang = trianguloLado + trianguloLado2 + trianguloBase;
+
+let areaTriang = (trianguloBase * trianguloH)/2;
+
+console.log("El area del triangulo es de: " + areaTriang + "cm");
+
+console.groupEnd();
