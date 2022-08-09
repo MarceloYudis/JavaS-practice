b4873052b5151cd7c37fa657313490a3d1730fe3
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-09 19:51:41 -0400

    calculo de los descuentos

--- descounts/proyectoDescuentos.js
+++ descounts/proyectoDescuentos.js
@@ -1,1 +1,15 @@
-alert("olanda")
+let preciOriginal = 150;
+const descuento = 30;
+let precioDescontado;
+
+function calcularDescuentos(precio, descuento) {
+    let porcentajeDescuento = 100 - descuento;
+    precioDescontado =  (( precio * porcentajeDescuento ))/100;
+
+    return precioDescontado
+}
+
+
+calcularDescuentos(preciOriginal, descuento)
+
+
