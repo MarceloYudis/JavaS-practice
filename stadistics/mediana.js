4df0ee920babd83b946a781d75cbfc7e70cfce17
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-16 20:36:44 -0400

    progresando

---
+++ stadistics/mediana.js
@@ -0,0 +1,22 @@
+const listita = [
+    900,
+    1000,
+    1100,
+    1000000
+];
+
+const mitadListita = listita.length / 2;
+
+function detectarPar(listita.length) {
+
+    if( listita % 2 === 0) {
+        return true;
+
+    } else {
+        return false;
+    }
+}
+
+function calculoMediana() {
+
+}
