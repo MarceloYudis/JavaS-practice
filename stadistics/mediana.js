ef82a8ca9ab2625d152d48d7275f93717f5b332a
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-16 20:46:13 -0400

    Calculo de mediana impar

--- stadistics/mediana.js
+++ stadistics/mediana.js
@@ -2,14 +2,17 @@ const listita = [
     900,
     1000,
     1100,
-    1000000
+    1000000,
+    8000,
+    7,
+    90
 ];
 
 const mitadListita = listita.length / 2;
 
-function detectarPar(listita.length) {
+function detectarPar(listita) {
 
-    if( listita % 2 === 0) {
+    if( listita.length % 2 === 0) {
         return true;
 
     } else {
@@ -18,5 +21,12 @@ function detectarPar(listita.length) {
 }
 
 function calculoMediana() {
+    const deteccionPar = detectarPar(listita);
+    if(deteccionPar) {
 
+    } else {
+        mediana = listita[parseInt(mitadListita)];
+        console.log(mediana);
+    }
 }
+calculoMediana(listita);
