3b3a71aeba2b8e94ec117ffbb1e54d5f08abb4c6
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-20 22:02:26 -0400

    calculo de la mediana completado

--- stadistics/armonicaMedia.js
+++ stadistics/armonicaMedia.js
@@ -9,11 +9,21 @@ const listado = [
 ]
 
 // media armonica = cantidad de elementos / ( suma de los reciprocos de cada elemento )
-
+let sumaReciproca = 0;
 for(i = 0; i < listado.length; i++) {
-    let sumaReciproca = sumaReciproca + 1/listado[i]; 
+    console.log("la lista " + listado[i]);
+
+    let valorReciproco = 1 / listado[i];
+    console.log("reciproco "+ valorReciproco);
+
     
+     sumaReciproca = sumaReciproca + valorReciproco;
+
+     console.log(" ->" +sumaReciproca);
+
 }
 
 
+
 const armonicaMedia = listado.length / sumaReciproca;
+console.log("MEDIANA ARMONICA " + armonicaMedia)
