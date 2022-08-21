cd1c92b8ca2bf7e40dd9b19b8513f433b6f69955
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-20 21:55:26 -0400

    inicio de proyecto MEDIANA ARMONICA

---
+++ stadistics/armonicaMedia.js
@@ -0,0 +1,19 @@
+const listado = [
+    1,
+    10,
+    33,
+    5,
+    77,
+    21,
+    11
+]
+
+// media armonica = cantidad de elementos / ( suma de los reciprocos de cada elemento )
+
+for(i = 0; i < listado.length; i++) {
+    let sumaReciproca = sumaReciproca + 1/listado[i]; 
+    
+}
+
+
+const armonicaMedia = listado.length / sumaReciproca;
