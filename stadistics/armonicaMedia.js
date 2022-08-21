bcfea8489a811a6f86f2afd74242652b7869bb2c
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-20 22:09:47 -0400

    puse todo en funciones

--- stadistics/armonicaMedia.js
+++ stadistics/armonicaMedia.js
@@ -1,29 +1,35 @@
-const listado = [
-    1,
-    10,
-    33,
-    5,
-    77,
-    21,
-    11
-]
+const listado = [];
 
 // media armonica = cantidad de elementos / ( suma de los reciprocos de cada elemento )
 let sumaReciproca = 0;
-for(i = 0; i < listado.length; i++) {
-    console.log("la lista " + listado[i]);
 
-    let valorReciproco = 1 / listado[i];
-    console.log("reciproco "+ valorReciproco);
 
-    
-     sumaReciproca = sumaReciproca + valorReciproco;
+function generadorDeListas(datillos) {
+    for(i = 0; i < datillos.length; i++) {
+        listado.push( datillos[i]); 
+    }
 
-     console.log(" ->" +sumaReciproca);
 
 }
 
+function calcularMedianaArmonica(listado) {
+    for(i = 0; i < listado.length; i++) {
+        console.log("la lista " + listado[i]);
+    
+        let valorReciproco = 1 / listado[i];
+        console.log("reciproco "+ valorReciproco);
+    
+        
+         sumaReciproca = sumaReciproca + valorReciproco;
+    
+         console.log(" -> " + sumaReciproca);
+    
+    }
+
+    const armonicaMedia = listado.length / sumaReciproca;
+    console.log("MEDIANA ARMONICA " + armonicaMedia)
+}
 
 
-const armonicaMedia = listado.length / sumaReciproca;
-console.log("MEDIANA ARMONICA " + armonicaMedia)
+generadorDeListas([1, 10, 3, 99, 54, 12, 28, 7]);
+calcularMedianaArmonica(listado);
